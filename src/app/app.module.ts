021077de63b108aae16bb7be9abedd0702a9eb94
Nav-ee-n <naveenfelix876@gmail.com>
2022-07-08 00:32:49 +0530

    Created View-Doctors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
+import { ViewDoctorsComponent } from './view-doctors/view-doctors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FrontpageComponent
+    FrontpageComponent,
+    ViewDoctorsComponent
   ],
   imports: [
     BrowserModule,
